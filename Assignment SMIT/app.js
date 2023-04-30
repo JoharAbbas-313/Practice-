dc18c945730da8d4225464251e60b813ed3a34af
JoharAbbas-313 <123437515+JoharAbbas-313@users.noreply.github.com>
2023-04-30 04:12:11 -0700

    Add files via upload

---
+++ Assignment SMIT/app.js
@@ -0,0 +1,126 @@
+// function greeting (){
+//   alert("Hello World")
+// }
+// greeting();
+
+// 2nd method 
+
+// function greeting(greet){
+// alert(greet)
+// }
+// greeting("general syntax");
+
+
+// function biodata(){
+//   alert("my name is johar abbas");
+// }
+
+// biodata();
+
+// parameter
+
+// function name(greet){
+// alert("My name is johar abbas")
+// }
+// name();
+
+// arrgument
+
+// function name(name){
+// alert(name);
+// } 
+
+// name("my name is abbas");
+
+// function add(a,b,c){
+// alert(a+b+c);
+// }
+// add(5,5,6);
+
+
+// function sum(a,b,c){
+//   alert(a-b-c);
+//   }
+//   sum(5,5,6);
+
+// function multiply(a,b,c){
+//   alert(a*b*c);
+// }
+// multiply(2,2);
+
+// function divided(a,b){
+// alert(a/b);
+// }
+// divided(10,9)
+
+
+// function add(a,b,c){
+// var z = a+b+c;
+// return z;
+// }
+// var g = add(2,3,4);
+// alert(g);
+
+
+// function sum(a,b,c){
+//   var g = a-b-c;
+//   return g;
+// }
+//  var t = sum(9,2,3);
+//  alert(t);
+
+
+// function multiply(a,b,c){
+//   var m = a*b*c;
+//   return m;
+// }
+// var t = multiply(2,2,3);
+// alert(t);
+
+// global scap method
+
+// var h = 15;
+// function add(a,b,c){
+// alert(h);
+//  var z = a+b+c;
+//  return z;
+// }
+
+// var t = add(2,3,4);
+// alert(t)
+
+
+// var j = 12;
+
+// function sum(a,b,c){
+//   alert(j);
+// var num = a-b-c;
+// return num;
+// }
+
+// var total = sum(3,4,2);
+// alert(total);
+
+// var j = 14;
+
+// function multiply(a,b){
+// alert(j);
+// var num = a*b;
+// return num;
+// }
+
+// var total = multiply(2,3);
+// alert(total);
+
+// local
+
+// function add(a,b){
+//   var h = 12;
+//   alert(h);
+//   var num = a+b;
+//   return num;
+
+// }
+
+// var total = add(2,3);
+// alert(total);
